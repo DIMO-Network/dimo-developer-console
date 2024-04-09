dab09780a8e06173fc17b772b0bdfae0780606fb
Santiago Botero Ruiz <boterosantiago6@gmail.com>
2024-04-08 22:58:46 -0500

    Fixed some global styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,32 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        gray: {
+          '50': '#f5f6f6',
+          '100': '#e4e8e9',
+          '200': '#ccd4d5',
+          '300': '#a9b5b7',
+          '400': '#7e8f92',
+          '500': '#637377',
+          '600': '#556166',
+          '700': '#495256',
+          '800': '#40484a',
+          '900': '#393e40',
+          '950': '#232729',
+        },
       },
     },
   },
