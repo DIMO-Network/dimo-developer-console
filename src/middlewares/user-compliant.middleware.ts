536472b2986e059711c3ffb6352284b93f65a854
Santiago Botero Ruiz <boterosantiago6@gmail.com>
2024-04-16 22:46:09 -0500

    Added some validations for user compliant

--- src/middlewares/user-compliant.middleware.ts
+++ src/middlewares/user-compliant.middleware.ts
@@ -5,11 +5,30 @@ export const UserCompliantMiddleware = async (request: NextRequest) => {
   const urlPath = request.nextUrl.pathname;
   const flow = request.nextUrl.searchParams.get('flow');
   const { value: isLogged } = cookies().get('logged') ?? {}; // TODO: IMPROVE VALIDATION
+  const { value: isCompliant } = cookies().get('compliant') ?? {}; // TODO: IMPROVE VALIDATION
   const { value: currentFlow = 'build-for' } = cookies().get('flow') ?? {}; // TODO: IMPROVE VALIDATION
   const header = new Headers();
 
   if (urlPath.startsWith('/sign-up') && Boolean(isLogged) && !flow) {
     header.set('redirect', `/sign-up?flow=${currentFlow}`);
+    return NextResponse.next({
+      request: { headers: header },
+    });
+  } else if (
+    !urlPath.startsWith('/sign-up') &&
+    Boolean(isLogged) &&
+    !isCompliant
+  ) {
+    header.set('redirect', `/sign-up`);
+    return NextResponse.next({
+      request: { headers: header },
+    });
+  } else if (
+    urlPath.startsWith('/sign-up') &&
+    Boolean(isLogged) &&
+    isCompliant
+  ) {
+    header.set('redirect', `/`);
     return NextResponse.next({
       request: { headers: header },
     });
