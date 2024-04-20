33c82d584a88e6085bb517e660eebb7720b65f4c
Santiago Botero Ruiz <boterosantiago6@gmail.com>
2024-04-19 20:11:52 -0500

    Added icons for menu

--- src/components/Icons/index.ts
+++ src/components/Icons/index.ts
@@ -2,10 +2,17 @@ export interface IconProps {
   className?: string;
 }
 
+export * from './APIIcon';
 export * from './CarRentalIcon';
 export * from './ChevronDownIcon';
 export * from './ComputerIcon';
 export * from './GitHubIcon';
 export * from './GoogleIcon';
+export * from './HomeIcon';
+export * from './IntegrationIcon';
+export * from './MonitorHeartIcon';
 export * from './PhoneIcon';
 export * from './PlusIcon';
+export * from './SettingsIcon';
+export * from './SummarizeIcon';
+export * from './SupportAgentIcon';
