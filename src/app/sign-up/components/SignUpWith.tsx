b71bf8276a8dff0b2e0a88deb4bfe8523ae13a42
Santiago Botero Ruiz <boterosantiago6@gmail.com>
2024-05-18 23:06:17 -0500

    Added checkbox to accept terms of service and privacy policy

--- src/app/sign-up/components/SignUpWith.tsx
+++ src/app/sign-up/components/SignUpWith.tsx
@@ -1,27 +1,41 @@
-
 import { Anchor } from '@/components/Anchor';
+import { CheckboxField } from '@/components/CheckboxField';
 import { SignInButtons } from '@/components/SignInButton';
+import { useState } from 'react';
 
 export const SignUpWith = () => {
+  const [acceptTerms, setAcceptTerms] = useState<boolean>(true);
+
+  const handleAcceptTerms = () => {
+    setAcceptTerms(!acceptTerms);
+  };
   return (
     <>
       <section className="sign-up__buttons">
-        <SignInButtons isSignIn={false} />
+        <SignInButtons isSignIn={false} disabled={!acceptTerms} />
       </section>
       <section className="sign-up__extra-links">
         <Anchor href="/sign-in" className="primary">
           Already have an account?
         </Anchor>
-        <p className="terms-caption">
-          by signing up, you are agreeing to our{' '}
-          <Anchor href="#" className="grey underline">
-            terms of service
-          </Anchor>{' '}
-          and{' '}
-          <Anchor href="#" className="grey underline">
-            privacy policy
-          </Anchor>
-        </p>
+        <div className="flex flex-row">
+          <CheckboxField
+            name="terms"
+            checked={acceptTerms}
+            onChange={handleAcceptTerms}
+            className={acceptTerms ? '' : 'required'}
+          />
+          <p className="terms-caption">
+            by signing up, you are agreeing to our{' '}
+            <Anchor href="#" className="grey underline">
+              terms of service
+            </Anchor>{' '}
+            and{' '}
+            <Anchor href="#" className="grey underline">
+              privacy policy
+            </Anchor>
+          </p>
+        </div>
       </section>
     </>
   );
--- src/components/SignInButton/SignInButtons.tsx
+++ src/components/SignInButton/SignInButtons.tsx
@@ -1,19 +1,31 @@
 'use client';
 import type { FC } from 'react';
 
-import React from 'react';
+import React, { useContext } from 'react';
 
 import { SignInButton } from '@/components/SignInButton';
 import { GitHubIcon, GoogleIcon } from '@/components/Icons';
 import { frontendUrl } from '@/config/default';
+import { NotificationContext } from '@/context/notificationContext';
 
 interface SignInButtonProps {
   isSignIn: boolean;
+  disabled: boolean;
 }
 
-export const SignInButtons: FC<SignInButtonProps> = ({ isSignIn }) => {
-  const handlerGitHubLogin = (app: string) => {
-    window.location.replace(`${frontendUrl}api/auth/authorize?app=${app}`);
+export const SignInButtons: FC<SignInButtonProps> = ({
+  isSignIn,
+  disabled,
+}) => {
+  const { setNotification } = useContext(NotificationContext);
+  const handlerLogin = (app: string) => {
+    if (disabled)
+      setNotification(
+        'You must accept terms of service and privacy policy',
+        'Terms of service',
+        'error'
+      );
+    else window.location.replace(`${frontendUrl}api/auth/authorize?app=${app}`);
   };
 
   return (
@@ -22,13 +34,13 @@ export const SignInButtons: FC<SignInButtonProps> = ({ isSignIn }) => {
         className="sm"
         isSignIn={isSignIn}
         Icon={GoogleIcon}
-        onClick={() => handlerGitHubLogin('google')}
+        onClick={() => handlerLogin('google')}
       />
       <SignInButton
         className="sm"
         isSignIn={isSignIn}
         Icon={GitHubIcon}
-        onClick={() => handlerGitHubLogin('github')}
+        onClick={() => handlerLogin('github')}
       />
     </>
   );
