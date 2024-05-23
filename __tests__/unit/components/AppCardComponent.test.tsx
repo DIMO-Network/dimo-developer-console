3bb696bc3f6b3b92ced70ab0807ace8803d56848
Santiago Botero Ruiz <boterosantiago6@gmail.com>
2024-05-22 22:33:06 -0500

    Added tests for AppCard component

---
+++ __tests__/unit/components/AppCardComponent.test.tsx
@@ -0,0 +1,17 @@
+import { AppCard } from '@/components/AppCard';
+import { appListMock } from '@/mocks/appList';
+import { render, screen } from '@testing-library/react';
+
+describe('AppCard', () => {
+  it('renders an app card', () => {
+    const [app] = appListMock;
+    const { container } = render(<AppCard {...app} />);
+
+    const appNameElm = screen.getByText('Test App');
+    const environmentElm = screen.getByText('Sandbox');
+
+    expect(appNameElm).toBeInTheDocument();
+    expect(environmentElm).toBeInTheDocument();
+    expect(container).toMatchSnapshot();
+  });
+});
