16ca0c0cf9de3c5d96df7e81cbb08422193ad21b
Santiago Botero Ruiz <boterosantiago6@gmail.com>
2024-05-26 20:06:56 -0500

    Refactored Title component to accept className and component name

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -1,13 +1,24 @@
 import { type FC, type ReactNode } from 'react';
 
 import './Title.css';
+import classNames from 'classnames';
 
 interface IProps {
   children: ReactNode;
+  component?: 'h1' | 'h2' | 'h3' | 'h4';
+  className?: string;
 }
 
-export const Title: FC<IProps> = ({ children }) => {
-  return <h1 className="title">{children}</h1>;
+export const Title: FC<IProps> = ({
+  children,
+  className = 'text-2xl',
+  component: Component = 'h1',
+}) => {
+  return (
+    <Component className={classNames('title', className)} role={Component}>
+      {children}
+    </Component>
+  );
 };
 
 export default Title;
