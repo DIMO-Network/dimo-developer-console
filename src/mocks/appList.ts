75b6f96da710802aecf9e0caa23847cb3644c8c4
Santiago Botero Ruiz <boterosantiago6@gmail.com>
2024-05-26 22:43:58 -0500

    Added signers

--- src/mocks/appList.ts
+++ src/mocks/appList.ts
@@ -5,30 +5,70 @@ export const appListMock: IApp[] = [
     name: 'Test App',
     scope: 'sandbox',
     client_id: '0x*HSD)BN*(SHJDM)S(IJD SIOPDJSN)abcded1EA',
+    signers: [
+      {
+        wallet: '0x1ABC7154748D1CE5144478CDEB574AE244B939B5',
+        key: 'abcdef12345abcdef12345abcdef12345abcdef12345',
+      },
+      {
+        wallet: '0x1ABC7154748D1CE5144478CDEB574AE244B939B5',
+        key: 'abcdef12345abcdef12345abcdef12345abcdef12345',
+      },
+    ],
   },
   {
     name: 'Fleet Management App',
     scope: 'production',
     client_id: '0x*HSD)BN*(SHJDM)S(IJD SIOPDJSN)abcded1EA',
+    signers: [
+      {
+        wallet: '0x1ABC7154748D1CE5144478CDEB574AE244B939B5',
+        key: 'abcdef12345abcdef12345abcdef12345abcdef12345',
+      },
+    ],
   },
   {
     name: 'Data Analytics',
     scope: 'production',
     client_id: '0x*HSD)BN*(SHJDM)S(IJD SIOPDJSN)abcded1EA',
+    signers: [
+      {
+        wallet: '0x1ABC7154748D1CE5144478CDEB574AE244B939B5',
+        key: 'abcdef12345abcdef12345abcdef12345abcdef12345',
+      },
+    ],
   },
   {
     name: 'Hello World',
     scope: 'production',
     client_id: '0x*HSD)BN*(SHJDM)S(IJD SIOPDJSN)abcded1EA',
+    signers: [
+      {
+        wallet: '0x1ABC7154748D1CE5144478CDEB574AE244B939B5',
+        key: 'abcdef12345abcdef12345abcdef12345abcdef12345',
+      },
+    ],
   },
   {
     name: 'Hello World',
     scope: 'production',
     client_id: '0x*HSD)BN*(SHJDM)S(IJD SIOPDJSN)abcded1EA',
+    signers: [
+      {
+        wallet: '0x1ABC7154748D1CE5144478CDEB574AE244B939B5',
+        key: 'abcdef12345abcdef12345abcdef12345abcdef12345',
+      },
+    ],
   },
   {
     name: 'Hello World',
     scope: 'production',
     client_id: '0x*HSD)BN*(SHJDM)S(IJD SIOPDJSN)abcded1EA',
+    signers: [
+      {
+        wallet: '0x1ABC7154748D1CE5144478CDEB574AE244B939B5',
+        key: 'abcdef12345abcdef12345abcdef12345abcdef12345',
+      },
+    ],
   },
 ];
--- src/types/app.ts
+++ src/types/app.ts
@@ -1,7 +1,14 @@
 type ENVIRONMENTS = 'production' | 'sandbox';
 
+export interface ISigner {
+  [k: string]: string;
+  wallet: string;
+  key: string;
+}
+
 export interface IApp {
   name: string;
   scope: ENVIRONMENTS;
   client_id: string;
+  signers: ISigner[];
 }
