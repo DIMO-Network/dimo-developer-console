364197376bc2be3148658f809b1ed89240caa696
Santiago Botero Ruiz <boterosantiago6@gmail.com>
2024-05-26 21:23:41 -0500

    Added some tests for redirect uri form

--- __tests__/unit/pages/app/details/[id]/RedirectUriForm.test.tsx
+++ __tests__/unit/pages/app/details/[id]/RedirectUriForm.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 
 import { RedirectUriForm } from '@/app/app/details/[id]/components/RedirectUriForm';
 
@@ -11,5 +11,25 @@ describe('RedirectUriForm', () => {
 
     expect(signerAddressInputElm).toBeInTheDocument();
     expect(addUriBtn).toBeInTheDocument();
+  });
+
+  it('renders the redirect uri validation texts', () => {
+    render(<RedirectUriForm />);
+
+    const signerAddressInputElm = screen.getByRole('redirect-url-input');
+
+    fireEvent.change(signerAddressInputElm, { target: { value: 'Test' } });
+    const invalidTextElm = screen.findByText('Invalid Redirect URI');
+
+    waitFor(() => {
+      expect(invalidTextElm).toBeInTheDocument();
+    });
+
+    fireEvent.change(signerAddressInputElm, { target: { value: '' } });
+    const emptyTextElm = screen.findByText('Please enter the redirect URI');
+
+    waitFor(() => {
+      expect(emptyTextElm).toBeInTheDocument();
+    });
   });
 });
