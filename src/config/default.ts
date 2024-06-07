44bf17e2e954b2fd93716e5737ac015ce9c36c9e
Santiago Botero Ruiz <boterosantiago6@gmail.com>
2024-06-06 22:34:53 -0500

    Changed urls for preview environment

--- src/config/default.ts
+++ src/config/default.ts
@@ -10,8 +10,10 @@ export const regions = [
   'Eastern Europe',
 ];
 
-export const frontendUrl = 'http://localhost:3000/';
-export const backendUrl = 'http://localhost:3001/';
+export const frontendUrl =
+  'https://dimo-developer-console-gula1lhu3-dimo-aa6dc82a.vercel.app/';
+export const backendUrl =
+  'https://dimo-developer-console-21io11fxt-dimo-aa6dc82a.vercel.app/';
 
 export const RAINBOW_PROJECT = {
   ID: '528803928611a7781fb6b23eaf232224',
