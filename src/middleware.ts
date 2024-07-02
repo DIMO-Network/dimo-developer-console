97e124beae98b0969ff98747af676bf21757007c
Santiago Botero Ruiz <boterosantiago6@gmail.com>
2024-07-01 22:41:48 -0500

    Fixed error when session has expired

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,6 +6,7 @@ import { isIn } from '@/utils/middlewareUtils';
 import { getUserByToken } from './services/user';
 import { LoggedUser } from '@/utils/loggedUser';
 import configuration from '@/config';
+import axios, { isAxiosError } from 'axios';
 
 const { LOGIN_PAGES, API_PATH, UNPROTECTED_PATHS } = configuration;
 
@@ -27,51 +28,76 @@ const mustBeAuthorize = (request: NextRequest, token: JWT | null) => {
   return isAPI && !isPublicAPIPath && !token;
 };
 
+const handleExpiredSession = (error: unknown) => {
+  if (isAxiosError(error)) {
+    const status = error.response?.status;
+    if (status === 401) {
+      const signOutUrl = `${configuration.frontendUrl}api/auth/signout`;
+      axios.post(signOutUrl);
+      return 'sign-in?error=expired';
+    }
+  }
+  return 'app?error=true';
+};
+
 export const middleware = async (
   request: NextRequest,
   event: NextFetchEvent
 ) => {
+  const hasError = request.nextUrl.searchParams.get('error');
   const token = await getToken({ req: request });
 
   // Setting the user up
-  const user = await getUserByToken();
-  request.user = new LoggedUser(user);
-  const isCompliant = request.user?.isCompliant ?? false;
-  const missingFlow = request.user?.missingFlow ?? null;
+  try {
+    if (token) {
+      const user = await getUserByToken();
+      request.user = new LoggedUser(user);
 
-  const isLoginPage = LOGIN_PAGES.includes(request.nextUrl.pathname);
-  const isAPIProtected = mustBeAuthorize(request, token);
-  const flow = request.nextUrl.searchParams.get('flow');
+      const isCompliant = request.user?.isCompliant ?? false;
+      const missingFlow = request.user?.missingFlow ?? null;
 
-  if (token && !isCompliant && !flow) {
-    return NextResponse.redirect(
-      new URL(`/sign-up?flow=${missingFlow}`, request.url),
-      {
-        status: 307,
+      const isLoginPage = LOGIN_PAGES.includes(request.nextUrl.pathname);
+      const isAPIProtected = mustBeAuthorize(request, token);
+      const flow = request.nextUrl.searchParams.get('flow');
+
+      if (token && !isCompliant && !flow) {
+        return NextResponse.redirect(
+          new URL(`/sign-up?flow=${missingFlow}`, request.url),
+          {
+            status: 307,
+          }
+        );
       }
-    );
-  }
 
-  if (isLoginPage && token && isCompliant) {
-    return NextResponse.redirect(new URL('/app', request.url), {
-      status: 307,
-    });
-  }
+      if (isLoginPage && token && isCompliant) {
+        return NextResponse.redirect(new URL('/app', request.url), {
+          status: 307,
+        });
+      }
 
-  if (isAPIProtected) {
-    return NextResponse.json(
-      {
-        message: 'Unauthorized Access',
-      },
-      { status: 401 }
-    );
-  }
+      if (isAPIProtected) {
+        return NextResponse.json(
+          {
+            message: 'Unauthorized Access',
+          },
+          { status: 401 }
+        );
+      }
 
-  if (!isLoginPage) {
-    return authMiddleware(request, event);
-  }
+      if (!isLoginPage) {
+        return authMiddleware(request, event);
+      }
+    }
 
-  return NextResponse.next();
+    return NextResponse.next();
+  } catch (error: unknown) {
+    const path = await handleExpiredSession(error);
+    const redirectUrl = `${configuration.frontendUrl}${path}`;
+    if (!hasError) {
+      return Response.redirect(redirectUrl);
+    }
+    return NextResponse.next();
+  }
 };
 
 export const config = {
