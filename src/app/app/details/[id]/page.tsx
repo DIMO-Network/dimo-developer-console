302ed7f7f09d332540fd6c8f4f6d08e21bbfa173
Santiago Botero Ruiz <boterosantiago6@gmail.com>
2024-07-03 22:52:21 -0500

    Added title to app details page

--- src/app/app/details/[id]/page.tsx
+++ src/app/app/details/[id]/page.tsx
@@ -1,49 +1,12 @@
-'use client';
-import { useEffect, useState } from 'react';
+import { Metadata } from 'next';
 
-import { AppSummary } from '@/app/app/details/[id]/components/AppSummary';
-import { BackButton } from '@/components/BackButton';
-import { Button } from '@/components/Button';
-import { getAppByID } from '@/actions/app';
-import { IApp } from '@/types/app';
-import { RedirectUriForm } from './components/RedirectUriForm';
-import { RedirectUriList } from '@/app/app/details/[id]/components/RedirectUriList';
-import { SignerForm } from '@/app/app/details/[id]/components/SignerForm';
-import { SignerList } from '@/app/app/details/[id]/components/SignerList';
-import { Title } from '@/components/Title';
-import { withNotifications } from '@/hoc';
+import { View } from '@/app/app/details/[id]/components/View';
+import configuration from '@/config';
 
-import './page.css';
-
-const AppDetailPage = ({ params: { id } }: { params: { id: string } }) => {
-  const [app, setApp] = useState<IApp>();
-  useEffect(() => {
-    getAppByID(id).then(setApp);
-  }, []);
-
-  return (
-    <div className="page">
-      <div className="summary">
-        <BackButton />
-        {app && <AppSummary app={app} />}
-      </div>
-      <div className="signers-content">
-        <Title component="h2">Signers</Title>
-        <SignerForm />
-      </div>
-      <div className="signers-table">{app && <SignerList app={app} />}</div>
-      <div className="redirect-uri-content">
-        <Title component="h2">Authorized Redirect URIs</Title>
-        <RedirectUriForm />
-      </div>
-      <div className="signers-table">
-        {app && <RedirectUriList app={app} />}
-      </div>
-      <div className="extra-actions">
-        <Button className="error-simple">Delete application</Button>
-      </div>
-    </div>
-  );
+export const metadata: Metadata = {
+  title: `Application Detail | ${configuration.appName}`,
 };
 
-export default withNotifications(AppDetailPage);
+const AppDetailPage = View;
+
+export default AppDetailPage;
