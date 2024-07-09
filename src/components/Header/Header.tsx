caecbbc2255ae9c264263ac85b5443fcde81eadc
Santiago Botero Ruiz <boterosantiago6@gmail.com>
2024-07-08 23:58:52 -0500

    Added func to open the buy credit modal when clicks the add credits button. Added balance from the smart contract

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,22 @@
-import { type FC } from 'react';
+import { useContext, type FC } from 'react';
 import { useSession } from 'next-auth/react';
 
+import { CreditsContext } from '@/context/creditsContext';
 import { PlusIcon } from '@/components/Icons';
+import { useContract } from '@/hooks';
 import { UserAvatar } from '@/components/UserAvatar';
 
 import './Header.css';
 
 export const Header: FC = () => {
+  const { balance } = useContract();
+  const { setIsOpen } = useContext(CreditsContext);
   const { data: session } = useSession();
   const { user: { name = '' } = {} } = session ?? {};
+
+  const handleOpenBuyCreditsModal = () => {
+    setIsOpen(true);
+  };
 
   return (
     <header className="header">
@@ -21,12 +29,16 @@ export const Header: FC = () => {
         <UserAvatar name={name ?? ''} />
         <div className="credits" role="credits-display">
           <div className="credits-info">
-            <p className="credit-amount">0</p>
+            <p className="credit-amount">{String(BigInt(balance))}</p>
             <p className="credit-text">Credits</p>
           </div>
-          <span className="btn-add-credits">
+          <button
+            className="btn-add-credits"
+            onClick={handleOpenBuyCreditsModal}
+            role="add-credits"
+          >
             <PlusIcon className="h-4 w-4" />
-          </span>
+          </button>
         </div>
       </div>
     </header>
