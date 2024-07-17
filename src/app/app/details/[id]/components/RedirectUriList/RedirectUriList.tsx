7431a066728cd6bd1f059ea98c9b19956ccb4518
Santiago Botero Ruiz <boterosantiago6@gmail.com>
2024-07-16 23:57:30 -0500

    Added loading modal to show the status of the transaction

--- src/app/app/details/[id]/components/RedirectUriList/RedirectUriList.tsx
+++ src/app/app/details/[id]/components/RedirectUriList/RedirectUriList.tsx
@@ -1,11 +1,11 @@
-import { useContext, type FC } from 'react';
+import { useState, type FC } from 'react';
 import { TrashIcon } from '@heroicons/react/24/outline';
 
+import { deleteMyRedirectUri, updateMyRedirectUri } from '@/actions/app';
 import { IRedirectUri } from '@/types/app';
+import { LoadingModal, LoadingProps } from '@/components/LoadingModal';
 import { Table } from '@/components/Table';
 import { Toggle } from '@/components/Toggle';
-import { deleteMyRedirectUri, updateMyRedirectUri } from '@/actions/app';
-import { NotificationContext } from '@/context/notificationContext';
 
 interface IProps {
   list: IRedirectUri[] | undefined;
@@ -13,7 +13,10 @@ interface IProps {
 }
 
 export const RedirectUriList: FC<IProps> = ({ list = [], refreshData }) => {
-  const { setNotification } = useContext(NotificationContext);
+  const [isOpened, setIsOpened] = useState<boolean>(false);
+  const [loadingStatus, setLoadingStatus] = useState<LoadingProps>();
+
+  const recordsToShow = list.filter(({ deleted }) => !deleted);
 
   const renderToggleStatus = ({ id, status }: IRedirectUri) => {
     return (
@@ -26,27 +29,31 @@ export const RedirectUriList: FC<IProps> = ({ list = [], refreshData }) => {
 
   const handleUpdateStatus = async (id: string, newStatus: boolean) => {
     try {
+      setIsOpened(true);
+      setLoadingStatus({
+        label: 'Updating the selected redirect URI',
+        status: 'loading',
+      });
       await updateMyRedirectUri(id, { status: newStatus });
+      setLoadingStatus({ label: 'Redirect URI updated', status: 'success' });
       refreshData();
     } catch (error: unknown) {
-      setNotification(
-        'Something wen wrong while deleting the redirect URI',
-        'Oops...',
-        'error'
-      );
+      setLoadingStatus({ label: 'Something went wrong', status: 'error' });
     }
   };
 
   const handleDeleteUri = async (id: string) => {
     try {
+      setIsOpened(true);
+      setLoadingStatus({
+        label: 'Deleting the selected redirect URI',
+        status: 'loading',
+      });
       await deleteMyRedirectUri(id);
+      setLoadingStatus({ label: 'Redirect URI deleted', status: 'success' });
       refreshData();
     } catch (error: unknown) {
-      setNotification(
-        'Something wen wrong while deleting the redirect URI',
-        'Oops...',
-        'error'
-      );
+      setLoadingStatus({ label: 'Something went wrong', status: 'error' });
     }
   };
 
@@ -57,12 +64,24 @@ export const RedirectUriList: FC<IProps> = ({ list = [], refreshData }) => {
       </button>
     );
   };
+
   return (
-    <Table
-      columns={[{ name: 'uri' }]}
-      data={list.filter(({ deleted }) => !deleted)}
-      actions={[renderToggleStatus, renderDeleteRedirectUriAction]}
-    />
+    <>
+      {recordsToShow.length > 0 && (
+        <>
+          <Table
+            columns={[{ name: 'uri' }]}
+            data={recordsToShow}
+            actions={[renderToggleStatus, renderDeleteRedirectUriAction]}
+          />
+          <LoadingModal
+            isOpen={isOpened}
+            setIsOpen={setIsOpened}
+            {...loadingStatus}
+          />
+        </>
+      )}
+    </>
   );
 };
 
--- src/app/app/details/[id]/components/SignerList/SignerList.tsx
+++ src/app/app/details/[id]/components/SignerList/SignerList.tsx
@@ -1,12 +1,12 @@
-import { useContext, type FC } from 'react';
+import { useState, type FC } from 'react';
 import { TrashIcon } from '@heroicons/react/24/outline';
 
 import { Button } from '@/components/Button';
 import { ContentCopyIcon } from '@/components/Icons';
-import { ISigner } from '@/types/app';
-import { NotificationContext } from '@/context/notificationContext';
-import { Table } from '@/components/Table';
 import { deleteMySigner } from '@/actions/app';
+import { ISigner } from '@/types/app';
+import { LoadingModal, LoadingProps } from '@/components/LoadingModal';
+import { Table } from '@/components/Table';
 
 interface IProps {
   list: ISigner[] | undefined;
@@ -14,7 +14,8 @@ interface IProps {
 }
 
 export const SignerList: FC<IProps> = ({ list = [], refreshData }) => {
-  const { setNotification } = useContext(NotificationContext);
+  const [isOpened, setIsOpened] = useState<boolean>(false);
+  const [loadingStatus, setLoadingStatus] = useState<LoadingProps>();
 
   const handleCopy = (value: string) => {
     void navigator.clipboard.writeText(value);
@@ -56,31 +57,40 @@ export const SignerList: FC<IProps> = ({ list = [], refreshData }) => {
 
   const handleDelete = async (id: string) => {
     try {
+      setIsOpened(true);
+      setLoadingStatus({
+        label: 'Deleting the selected API key',
+        status: 'loading',
+      });
       await deleteMySigner(id);
+      setLoadingStatus({ label: 'API key deleted', status: 'success' });
       refreshData();
     } catch (error: unknown) {
-      setNotification(
-        'Something wen wrong while deleting the API key',
-        'Oops...',
-        'error'
-      );
+      setLoadingStatus({ label: 'Something went wrong', status: 'error' });
     }
   };
 
   return (
     <>
       {list && list.length > 0 && (
-        <Table
-          columns={[
-            {
-              name: 'api_key',
-              label: 'API Key',
-              render: (item) => renderWithCopy('api_key', item),
-            },
-          ]}
-          data={list.filter(({ deleted }) => !deleted)}
-          actions={[renderTestAuthenticationAction, renderDeleteSignerAction]}
-        />
+        <>
+          <Table
+            columns={[
+              {
+                name: 'api_key',
+                label: 'API Key',
+                render: (item) => renderWithCopy('api_key', item),
+              },
+            ]}
+            data={list.filter(({ deleted }) => !deleted)}
+            actions={[renderTestAuthenticationAction, renderDeleteSignerAction]}
+          />
+          <LoadingModal
+            isOpen={isOpened}
+            setIsOpen={setIsOpened}
+            {...loadingStatus}
+          />
+        </>
       )}
     </>
   );
