78399761425d1f19c5b85638f6284a32db89a340
Santiago Botero Ruiz <boterosantiago6@gmail.com>
2024-07-22 19:45:24 -0500

    Removed API Reference, marked some menu options as disabled, and added external links for some of the menu options.

--- src/config/navigation.ts
+++ src/config/navigation.ts
@@ -3,7 +3,6 @@ import { ArrowLeftStartOnRectangleIcon } from '@heroicons/react/24/outline';
 import { signOut } from 'next-auth/react';
 
 import {
-  APIIcon,
   HomeIcon,
   IntegrationIcon,
   MonitorHeartIcon,
@@ -17,37 +16,41 @@ export const mainMenu = [
     label: 'Home',
     icon: HomeIcon,
     iconClassName: 'h-5 w-5',
-    link: '/app/list',
+    link: '/app',
+    external: false,
+    disabled: false,
   },
   {
     label: 'Integrations',
     icon: IntegrationIcon,
     iconClassName: 'h-5 w-5 fill-white stroke-white stroke-1',
     link: '/integrations',
+    external: false,
+    disabled: true,
   },
   {
     label: 'Support',
     icon: SupportAgentIcon,
     iconClassName: 'h-5 w-5',
-    link: '/support',
-  },
-  {
-    label: 'API Reference',
-    icon: APIIcon,
-    iconClassName: 'h-5 w-5',
-    link: '/api-reference',
+    link: 'https://discord.com/channels/892438668453740634/940719111971946546',
+    external: true,
+    disabled: false,
   },
   {
     label: 'Documentation',
     icon: SummarizeIcon,
     iconClassName: 'h-5 w-5',
-    link: '/docs',
+    link: 'https://docs.dimo.zone/developer-platform',
+    external: true,
+    disabled: false,
   },
   {
     label: 'API Status',
     icon: MonitorHeartIcon,
     iconClassName: 'h-5 w-5',
     link: '/api-status',
+    external: false,
+    disabled: true,
   },
 ];
 
@@ -57,11 +60,15 @@ export const bottomMenu = [
     icon: ArrowLeftStartOnRectangleIcon as FC,
     iconClassName: 'h-5 w-5 fill-grey-200',
     link: () => signOut({ callbackUrl: '/sign-in' }),
+    external: false,
+    disabled: false,
   },
   {
     label: 'Settings',
     icon: SettingsIcon,
     iconClassName: 'h-5 w-5 fill-grey-200',
     link: '/settings',
+    external: false,
+    disabled: false,
   },
 ];
