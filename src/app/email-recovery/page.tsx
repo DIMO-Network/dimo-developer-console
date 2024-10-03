556619f5d90b5e2b7b3945a35bc3b21b1945d392
Eduardo Rodriguez <eduardo.rodriguezz@outlook.com>
2024-10-03 23:16:19 +0200

    [DEVR-118] fix section naming

--- src/app/email-recovery/page.tsx
+++ src/app/email-recovery/page.tsx
@@ -5,7 +5,7 @@ import { View } from '@/app/email-recovery/components/View';
 import configuration from '@/config';
 
 export const metadata: Metadata = {
-  title: `Sign In | ${configuration.appName}`,
+  title: `Email Recovery | ${configuration.appName}`,
   description:
     'Welcome to DIMO Developer Console Sign In. Access your developer dashboard securely to manage applications, and explore advanced the DIMO tools. Start coding and optimizing today!',
 };
