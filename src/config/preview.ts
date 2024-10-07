74d41ab7090843cdcb0b14a33e5674dacbe71233
Eduardo Rodriguez <eduardo.rodriguezz@outlook.com>
2024-10-07 20:49:42 +0200

    [fix] update domains

--- src/config/preview.ts
+++ src/config/preview.ts
@@ -1,3 +1,3 @@
-export const backendUrl = 'https://console-api.dimo.xyz/';
+export const backendUrl = 'https://console-api.dimo.org/';
 
-export const frontendUrl = 'https://console.dimo.xyz/';
+export const frontendUrl = 'https://console.dimo.org/';
