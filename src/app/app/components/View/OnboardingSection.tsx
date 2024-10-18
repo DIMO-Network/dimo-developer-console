16f77866596a1dae05be95ac6e26dd026f8cda81
Laura Deng <laura@dimo.zone>
2024-10-18 16:44:31 -0400

    refactored components to use button title and card compoenents

--- src/app/app/components/View/OnboardingSection.tsx
+++ src/app/app/components/View/OnboardingSection.tsx
@@ -1,17 +1,24 @@
-import React from 'react';
+import { FC } from 'react';
+import { Card } from '@/components/Card';
+import { Title } from '@/components/Title';
+import { Button } from '@/components/Button';
 
-const OnboardingSection: React.FC = () => {
+const OnboardingSection: FC = () => {
     return (
-        <div className="onboarding-section">
-            <h3>What are DIMO Credits (DCX)?</h3>
+        <Card className="onboarding-card">
+            <Title component="h3" className="text-lg">
+                What are DIMO Credits (DCX)?
+            </Title>
             <p>
                 DCX is a stablecoin that’s pegged to one-tenth of a cent (0.001 USD), and is essential for all developers building on DIMO.
                 For DIMO protocol fees, spending DCX or $DIMO is required. The only way to get DCX is by trading in $DIMO, which converts at the market price.
                 This provides developers a way to lock in DCX at rates without being influenced by price fluctuation.
                 DCX cannot be converted back into $DIMO and when it is spent, it is burned forever.
                 For more details on DCX, check out our explanation on DCX & $DIMO.
+
             </p>
-        </div>
+            <Button className="purchase-dcx-button">Purchase DCX</Button>
+        </Card>
     );
 };
 
