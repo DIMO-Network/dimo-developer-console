2c5413eca050fb32873c2a3f600850aa668144ff
Santiago Botero Ruiz <boterosantiago6@gmail.com>
2024-11-04 20:49:39 -0500

    Moved app list component to a separate component

---
+++ src/app/app/list/components/AppList/AppList.tsx
@@ -0,0 +1,50 @@
+import { PlusIcon } from "@heroicons/react/24/outline";
+import { useSession } from "next-auth/react";
+import { type FC } from "react";
+import { useRouter } from "next/navigation";
+
+import { TeamRoles } from "@/types/team";
+import { IApp } from "@/types/app";
+import { Anchor } from "@/components/Anchor";
+import { AppCard } from "@/components/AppCard";
+import { Button } from "@/components/Button";
+
+interface IProps {
+  apps: IApp[];
+}
+export const AppList: FC<IProps> = ({ apps }) => {
+  const router = useRouter();
+  const { data: session } = useSession();
+  const { user: { role = '' } = {} } = session ?? {};
+
+  const handleCreateApp = () => {
+    router.push('/app/create');
+  };
+
+  const renderItem = (app: IApp) => {
+    return (
+      <Anchor href={`/app/details/${app?.id}`} key={app?.id}>
+        <AppCard className="hover:!border-white" {...app} />
+      </Anchor>
+    );
+  };
+
+  return <div className="app-list-content">
+    <div className="description">
+      <p className="title">Your applications</p>
+      {role === TeamRoles.OWNER && (
+        <Button
+          className="primary px-3 with-icon"
+          onClick={handleCreateApp}
+        >
+          <PlusIcon className="w-4 h-4" />
+          Create new
+        </Button>
+      )}
+    </div>
+    <div className="app-list">{apps.map(renderItem)}</div>
+  </div>;
+
+};
+
+export default AppList;
--- src/app/app/list/components/View/View.tsx
+++ src/app/app/list/components/View/View.tsx
@@ -1,26 +1,17 @@
 'use client';
 
 import { FC, useEffect, useState } from 'react';
-import { PlusIcon } from '@heroicons/react/24/outline';
-import { useRouter } from 'next/navigation';
-import { useSession } from 'next-auth/react';
 
-import { Anchor } from '@/components/Anchor';
-import { AppCard } from '@/components/AppCard';
-import { Button } from '@/components/Button';
 import { getApps } from '@/actions/app';
 import { IApp } from '@/types/app';
 import { Loader } from '@//components/Loader';
-import { TeamRoles } from '@/types/team';
+import { AppList } from '@/app/app/list/components/AppList';
 
 import './View.css';
 
 export const View: FC = () => {
   const [apps, setApps] = useState<IApp[]>([]);
   const [isLoadingPage, setIsLoadingPage] = useState<boolean>(true);
-  const router = useRouter();
-  const { data: session } = useSession();
-  const { user: { name = '', role = '' } = {} } = session ?? {};
 
   useEffect(() => {
     setIsLoadingPage(true);
@@ -29,42 +20,15 @@ export const View: FC = () => {
       .finally(() => setIsLoadingPage(false));
   }, []);
 
-  const handleCreateApp = () => {
-    router.push('/app/create');
-  };
-
-  const renderItem = (app: IApp) => {
-    return (
-      <Anchor href={`/app/details/${app?.id}`} key={app?.id}>
-        <AppCard className="hover:!border-white" {...app} />
-      </Anchor>
-    );
-  };
-
   return (
     <>
       {isLoadingPage && <Loader isLoading={true} />}
       {!isLoadingPage && (
         <div className="app-list-page">
           <div className="welcome-message">
-            <p className="title">Welcome {name}</p>
+            <p className="title">Welcome to DIMO Developer Consoleh</p>
           </div>
-
-          <>
-            <div className="description">
-              <p className="title">Your applications</p>
-              {role === TeamRoles.OWNER && (
-                <Button
-                  className="primary px-3 with-icon"
-                  onClick={handleCreateApp}
-                >
-                  <PlusIcon className="w-4 h-4" />
-                  Create new
-                </Button>
-              )}
-            </div>
-            <div className="app-list">{apps.map(renderItem)}</div>
-          </>
+          {apps.length && <AppList apps={apps} />}
         </div>
       )}
     </>
