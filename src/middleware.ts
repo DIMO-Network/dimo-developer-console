a2d214ec5ab953bf933ca1102b3ac261eac88a78
Santiago Botero Ruiz <boterosantiago6@gmail.com>
2024-11-05 21:19:18 -0500

    Fixed login page when user is already logged in

--- src/middleware.ts
+++ src/middleware.ts
@@ -51,67 +51,90 @@ const handleExpiredSession = (error: unknown, isLoginPage: boolean) => {
   return isLoginPage ? 'sign-in?error=true' : 'app?error=true';
 };
 
+const validatePrivateSession = async (
+  request: NextRequest,
+  event: NextFetchEvent,
+) => {
+  const user = await getUserByToken();
+  const subOrganization = await getUserSubOrganization(
+    user.company_email_owner ?? user.email,
+  );
+  request.user = new LoggedUser(user, subOrganization);
+
+  const isValidationPage = VALIDATION_PAGES.includes(request.nextUrl.pathname);
+  const isLoginPage = LOGIN_PAGES.includes(request.nextUrl.pathname);
+
+  const isCompliant = request.user?.isCompliant ?? false;
+  const missingFlow = request.user?.missingFlow ?? null;
+  const flow = request.nextUrl.searchParams.get('flow');
+
+  if ((isValidationPage || isLoginPage) && isCompliant) {
+    return NextResponse.redirect(new URL('/app', request.url), {
+      status: 307,
+    });
+  }
+
+  if (!isCompliant && !flow) {
+    return NextResponse.redirect(
+      new URL(`/sign-up?flow=${missingFlow}`, request.url),
+      {
+        status: 307,
+      },
+    );
+  }
+
+  if (!isLoginPage) {
+    return authMiddleware(request, event);
+  }
+  return NextResponse.next();
+
+};
+
+
+const validatePublicSession = async (
+  request: NextRequest,
+) => {
+  const token = await getToken({ req: request });
+  const isLoginPage = LOGIN_PAGES.includes(request.nextUrl.pathname);
+  const isAPIProtected = mustBeAuthorize(request, token);
+  const isAPI = request.nextUrl.pathname.startsWith(API_PATH);
+
+  if (isAPIProtected && isAPI) {
+    return NextResponse.json(
+      {
+        message: 'Unauthorized Access',
+      },
+      { status: 401 },
+    );
+  }
+
+  if (!isLoginPage) {
+    return NextResponse.redirect(new URL('/sign-in', request.url), {
+      status: 307,
+    });
+  }
+
+  return NextResponse.next();
+
+};
+
+
 export const middleware = async (
   request: NextRequest,
   event: NextFetchEvent,
 ) => {
   const hasError = request.nextUrl.searchParams.get('error');
   const token = await getToken({ req: request });
-  const isAPIProtected = mustBeAuthorize(request, token);
-  const isAPI = request.nextUrl.pathname.startsWith(API_PATH);
   const isLoginPage = LOGIN_PAGES.includes(request.nextUrl.pathname);
-  const isValidationPage = VALIDATION_PAGES.includes(request.nextUrl.pathname);
 
   // Setting the user up
   try {
     if (token) {
-      const user = await getUserByToken();
-      const subOrganization = await getUserSubOrganization(
-        user.company_email_owner ?? user.email,
-      );
-      request.user = new LoggedUser(user, subOrganization);
-
-      const isCompliant = request.user?.isCompliant ?? false;
-      const missingFlow = request.user?.missingFlow ?? null;
-      const flow = request.nextUrl.searchParams.get('flow');
-
-      if (isValidationPage && isCompliant) {
-        return NextResponse.redirect(new URL('/app', request.url), {
-          status: 307,
-        });
-      }
-
-      if (!isCompliant && !flow) {
-        return NextResponse.redirect(
-          new URL(`/sign-up?flow=${missingFlow}`, request.url),
-          {
-            status: 307,
-          },
-        );
-      }
-
-      if (!isLoginPage) {
-        return authMiddleware(request, event);
-      }
-      return NextResponse.next();
+      return validatePrivateSession(request, event);
     }
 
-    if (isAPIProtected && isAPI) {
-      return NextResponse.json(
-        {
-          message: 'Unauthorized Access',
-        },
-        { status: 401 },
-      );
-    }
+    return validatePublicSession(request);
 
-    if (!isLoginPage) {
-      return NextResponse.redirect(new URL('/sign-in', request.url), {
-        status: 307,
-      });
-    }
-
-    return NextResponse.next();
   } catch (error: unknown) {
     console.error('Middleware error:', error);
     let path = handleConnectionError(error, isLoginPage);
