1271a7b04445dd6f36d200b6d09a8a83a021a61e
Eduardo Rodriguez <eduardo.rodriguezz@outlook.com>
2024-11-19 01:31:03 +0100

    lint issues

--- src/services/wallet.ts
+++ src/services/wallet.ts
@@ -8,21 +8,18 @@ const dimoApiClient = xior.create({
 });
 
 export const getCurrentDimoPrice = async (): Promise<number> => {
-  'use cache';
   const { data } =
     await dimoApiClient.get<ICoinMarketTokenResponse>('/api/crypto/DIMO');
   return data.data.DIMO[0].quote.USD.price;
 };
 
 export const getCurrentPolPrice = async (): Promise<number> => {
-  'use cache';
   const { data } =
     await dimoApiClient.get<ICoinMarketTokenResponse>('/api/crypto/POL');
   return data.data.POL[0].quote.USD.price;
 };
 
 export const getCurrentWMaticPrice = async (): Promise<number> => {
-  'use cache';
   const { data } =
     await dimoApiClient.get<ICoinMarketTokenResponse>('/api/crypto/WMATIC');  
   return data.data.WMATIC[0].quote.USD.price;
