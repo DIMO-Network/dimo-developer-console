d805a1eecc08ca334550a1bbe23ae5ffe596d2ae
Eduardo Rodriguez <eduardo.rodriguezz@outlook.com>
2025-02-10 23:28:47 +0100

    remove CSP

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,5 @@
 import { withSentryConfig } from '@sentry/nextjs';
 
-const cspHeader = `
-    default-src 'self';
-    script-src 'self' 'unsafe-inline' 'unsafe-eval' sentry.io cloudflareinsights.com vercel.live;
-    frame-src 'self' *.turnkey.com;
-    connect-src 'self' sentry.io cloudflareinsights.com;
-    img-src 'self' data:;
-    style-src 'self' 'unsafe-inline' cloudflareinsights.com;
-    font-src 'self' cloudflareinsights.com;
-    object-src 'none';`;
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
@@ -26,20 +16,7 @@ const nextConfig = {
     fetches: {
       fullUrl: true,
     },
-  },
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: cspHeader.replace(/\n/g, ''),
-          },
-        ],
-      },
-    ];
-  },
+  }
 };
 
 export default withSentryConfig(nextConfig, {
