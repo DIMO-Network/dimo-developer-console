ad0e58cac94c56e821acce719e52e486c2922cdf
Eduardo Rodriguez <eduardo.rodriguezz@outlook.com>
2025-02-10 21:21:26 +0100

    remove commented code on config

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,5 @@
 import { withSentryConfig } from '@sentry/nextjs';
 
-
-
-// const cspHeader = `
-// script-src: 'self' 'unsafe-eval' 'unsafe-inline';
-//     style-src: 'self' 'unsafe-inline';
-//     img-src 'self' blob: data:;
-//     font-src 'self';
-//     object-src 'none';
-//     base-uri 'self';
-//     form-action 'self';
-//     frame-ancestors 'none';
-//     upgrade-insecure-requests;`;
-
-//const headers = ["default-src 'self';", "script-src 'self' 'unsafe-eval';", "style-src 'self' 'unsafe-inline';", "img-src 'self' data:;"];
-
 const cspHeader = `
     default-src 'self';
     script-src 'self' 'unsafe-eval' 'unsafe-inline';
