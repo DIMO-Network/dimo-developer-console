6eb6ac647b412cd119bcc10891f6f35b1f7e9ee8
Eduardo Rodriguez <eduardo.rodriguezz@outlook.com>
2025-02-10 21:13:43 +0100

    change middleware and csp policy

--- src/middleware.ts
+++ src/middleware.ts
@@ -141,5 +141,20 @@ export const middleware = async (request: NextRequest, event: NextFetchEvent) =>
 };
 
 export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico|images|api/auth).*)'],
-};
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    {
+      source: '/((?!_next/static|_next/image|favicon.ico|images|api/auth).*)',
+      missing: [
+        { type: 'header', key: 'next-router-prefetch' },
+        { type: 'header', key: 'purpose', value: 'prefetch' },
+      ],
+    },
+  ],
+}
