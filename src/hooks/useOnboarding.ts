82c2b114c59678c846e14c06a39b1cd45bcdb0aa
Neil Devas <neil.devas97@gmail.com>
2025-03-10 16:01:45 -0400

    Update useOnboarding.ts

--- src/hooks/useOnboarding.ts
+++ src/hooks/useOnboarding.ts
@@ -1,15 +1,11 @@
 'use client';
-import { type CTA } from '@/app/app/list/components/Banner';
 
 import { useContext, useEffect, useState } from 'react';
-import { useRouter } from 'next/navigation';
-import { useSession } from 'next-auth/react';
 
 import { CreditsContext } from '@/context/creditsContext';
 import { getApps } from '@/actions/app';
 import { getWorkspace } from '@/actions/workspace';
 import { IApp } from '@/types/app';
-import { isOwner } from '@/utils/user';
 import { IWorkspace } from '@/types/workspace';
 import { useContractGA } from '@/hooks';
 import * as Sentry from '@sentry/nextjs';
@@ -18,18 +14,14 @@ import { GlobalAccountAuthContext } from '@/context/GlobalAccountAuthContext';
 export const useOnboarding = () => {
   const [apps, setApps] = useState<IApp[]>([]);
   const [workspace, setWorkspace] = useState<IWorkspace>();
-  const [cta, setCta] = useState<CTA>();
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [balance, setBalance] = useState<number>(0);
-  const { getDcxBalance, getDimoBalance } = useContractGA();
+  const { getDcxBalance } = useContractGA();
   const { setIsOpen } = useContext(CreditsContext);
-  const { data: session } = useSession();
-  const { user: { role = '' } = {} } = session ?? {};
   const { hasSession } = useContext(GlobalAccountAuthContext);
 
   const loadAppsAndWorkspace = async (): Promise<void> => {
     try {
-      console.log('load apps and workspace called');
       setIsLoading(true);
       await new Promise(resolve => setTimeout(resolve, 3000));
       const { data: createdApps } = await getApps();
@@ -47,35 +39,10 @@ export const useOnboarding = () => {
     }
   };
 
-  const setCtas = async () => {
-    if (isOwner(role)) {
-      const [balanceDCX, balanceDimo] = await Promise.all([
-        getDcxBalance(),
-        getDimoBalance(),
-      ]);
-      if (!(balanceDCX > 0 || balanceDimo > 0)) {
-        setCta({
-          label: 'Add credits',
-          onClick: handleOpenBuyCreditsModal,
-        });
-      } else if (apps.length === 0) {
-        setCta({
-          label: 'Create an app',
-          onClick: handleCreateApp,
-        });
-      }
-    } else setCta(undefined);
-  };
-
   useEffect(() => {
     if (!hasSession) return;
     void loadAppsAndWorkspace();
   }, [hasSession]);
-
-  useEffect(() => {
-    if (!hasSession) return;
-    void setCtas();
-  }, [apps, role, hasSession]);
 
   const handleOpenBuyCreditsModal = () => {
     setIsOpen(true);
@@ -84,7 +51,6 @@ export const useOnboarding = () => {
   return {
     balance,
     apps,
-    cta,
     isLoading,
     setIsLoading,
     handleOpenBuyCreditsModal,
