28d5495bc7fdee2098929b10f1ec91126149d4be
Neil Devas <neil.devas97@gmail.com>
2025-03-19 15:57:21 -0400

    Update page.tsx

--- src/app/license/details/[tokenId]/page.tsx
+++ src/app/license/details/[tokenId]/page.tsx
@@ -1,11 +1,11 @@
 import { Metadata } from 'next';
 
 import configuration from '@/config';
-import {View as LicenseDetailsPage} from '@/app/license/details/[tokenId]/components/View';
+import {View} from '@/app/license/details/[tokenId]/components/View';
 
 export const metaData: Metadata = {
   title: `License Details | ${configuration.appName}`
 };
 
-
+const LicenseDetailsPage = View;
 export default LicenseDetailsPage;
