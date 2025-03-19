403d5fd18d9bebdb6d82070b4661a3714a0c6676
Neil Devas <neil.devas97@gmail.com>
2025-03-19 11:20:22 -0400

    Update useOnboarding.ts

--- src/hooks/useOnboarding.ts
+++ src/hooks/useOnboarding.ts
@@ -1,11 +1,13 @@
 'use client';
 
 import { useContext, useEffect, useState } from 'react';
+import { useSession } from 'next-auth/react';
 
 import { CreditsContext } from '@/context/creditsContext';
 import { getApps } from '@/actions/app';
 import { getWorkspace } from '@/actions/workspace';
 import { IApp } from '@/types/app';
+import { isOwner } from '@/utils/user';
 import { IWorkspace } from '@/types/workspace';
 import { useContractGA } from '@/hooks';
 import * as Sentry from '@sentry/nextjs';
@@ -18,6 +20,8 @@ export const useOnboarding = () => {
   const [balance, setBalance] = useState<number>(0);
   const { getDcxBalance } = useContractGA();
   const { setIsOpen } = useContext(CreditsContext);
+  const { data: session } = useSession();
+  const { user: { role = '' } = {} } = session ?? {};
   const { hasSession } = useContext(GlobalAccountAuthContext);
 
   const loadAppsAndWorkspace = async (): Promise<void> => {
