24046259564d68e33ddf80be8bc5d1e491c9f2cf
Neil Devas <neil.devas97@gmail.com>
2025-03-26 16:06:05 -0400

    updates

--- src/layouts/AuthorizedLayout/AuthorizedLayout.tsx
+++ src/layouts/AuthorizedLayout/AuthorizedLayout.tsx
@@ -1,8 +1,9 @@
 'use client';
-import React from 'react';
-
+import React, { useState } from 'react';
+import { MenuButton } from '@/components/Menu/MenuButton';
 import { withCredits, withNotifications, withGlobalAccounts, withApollo } from '@/hoc';
-
+import { Header } from '@/components/Header';
+import { Menu } from '@/components/Menu';
 import './AuthorizedLayout.css';
 
 const View = withNotifications(
@@ -17,6 +18,32 @@ export const AuthorizedLayout = ({
   children,
 }: Readonly<{
   children: React.ReactNode;
-}>) => <View>{children}</View>;
+}>) => {
+  const [isFullMenuOpen, setIsFullMenuOpen] = useState(false);
+  return (
+    <View>
+      <div className="main">
+        <div className="sidebar-container">
+          <Menu />
+        </div>
+        <div className="app-content">
+          <div className="header-container">
+            <div className="menu-header-button">
+              <MenuButton onClick={() => setIsFullMenuOpen(true)} />
+            </div>
+            <Header />
+          </div>
+          {isFullMenuOpen && (
+            <div className={'full-screen-menu-container'}>
+              <Menu onClose={() => setIsFullMenuOpen(false)} />
+            </div>
+          )}
+
+          <main className="page-content">{children}</main>
+        </div>
+      </div>
+    </View>
+  );
+};
 
 export default AuthorizedLayout;
