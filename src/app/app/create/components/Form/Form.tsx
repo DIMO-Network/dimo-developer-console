3029eeb3e8c777b8c05f3d76896da821ff296a51
Neil Devas <neil.devas97@gmail.com>
2025-03-26 17:09:24 -0400

    create license (#145)
    * updates
    
    * lint
    
    * Update Banner.tsx
    
    * Update useTransactions.ts

--- src/app/app/create/components/Form/Form.tsx
+++ src/app/app/create/components/Form/Form.tsx
@@ -1,50 +1,32 @@
-'use client';
 import { FC, useContext, useState } from 'react';
 import { useForm } from 'react-hook-form';
-import { encodeFunctionData } from 'viem';
-import { useRouter } from 'next/navigation';
-import { utils } from 'web3';
 
-import { isEmpty } from 'lodash';
 import * as Sentry from '@sentry/nextjs';
 
 import { Button } from '@/components/Button';
-import { createApp } from '@/actions/app';
-import { createWorkspace } from '@/actions/workspace';
 import { decodeHex } from '@/utils/formatHex';
 import { IAppWithWorkspace } from '@/types/app';
-import { IDesiredTokenAmount, ITokenBalance } from '@/types/wallet';
 import { IWorkspace } from '@/types/workspace';
 import { Label } from '@/components/Label';
 import { LoadingProps } from '@/components/LoadingModal';
 import { NotificationContext } from '@/context/notificationContext';
 import { TextError } from '@/components/TextError';
 import { TextField } from '@/components/TextField';
-import { useContractGA, useGlobalAccount } from '@/hooks';
+import { useGlobalAccount, usePayLicenseFee, useMintLicense } from '@/hooks';
 
 import configuration from '@/config';
-import DimoABI from '@/contracts/DimoTokenContract.json';
-import DimoCreditsABI from '@/contracts/DimoCreditABI.json';
-import DimoLicenseABI from '@/contracts/DimoLicenseContract.json';
 import { Loading } from '@/components/Loading';
 import './Form.css';
-
-const { CONTRACT_METHODS } = configuration;
-
-const { DCX_IN_USD = 0.001 } = process.env;
 
 interface IProps {
   workspace?: IWorkspace;
   onSuccess: () => void;
 }
 
-export const Form: FC<IProps> = ({ workspace, onSuccess }) => {
+export const Form: FC<IProps> = ({ onSuccess }) => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const { setNotification } = useContext(NotificationContext);
-  const { currentUser, getCurrentDcxBalance } = useGlobalAccount();
-  const { checkEnoughBalance, getDesiredTokenAmount, processTransactions } =
-    useContractGA();
-  const router = useRouter();
+  const { currentUser } = useGlobalAccount();
   const {
     formState: { errors },
     handleSubmit,
@@ -55,32 +37,28 @@ export const Form: FC<IProps> = ({ workspace, onSuccess }) => {
     reValidateMode: 'onChange',
   });
   const [loadingStatus, setLoadingStatus] = useState<LoadingProps>();
+  const payLicenseFee = usePayLicenseFee();
+  const mintLicense = useMintLicense();
 
   const onSubmit = async () => {
     try {
       setIsLoading(true);
       setLoadingStatus({
-        label: 'Preparing to license the application...',
+        label: 'Preparing to create the developer license',
         status: 'loading',
       });
-
-      const desiredTokenAmount = await getDesiredTokenAmount();
-      const enoughBalance = await checkEnoughBalance();
-      const transactions = [];
-
-      if (!enoughBalance.dcx && !enoughBalance.dimo)
-        return setNotification('Insufficient DIMO or DCX balance', 'Oops...', 'error');
-
-      if (!enoughBalance.dcx) {
-        transactions.push(...(await mintDCX(desiredTokenAmount, enoughBalance)));
+      const { reason } = await payLicenseFee();
+      if (reason) {
+        return setNotification(reason, 'Oops...', 'error');
       }
-
-      transactions.push(...(await prepareIssueInDC(desiredTokenAmount, enoughBalance)));
-      if (transactions.length) {
-        await processTransactions(transactions);
-      }
-
-      await handleCreateApp();
+      const { workspace } = getValues();
+      await createDeveloperLicense(workspace);
+      setNotification(
+        'Developer license created! Refresh the page to see your changes.',
+        'Success',
+        'success',
+      );
+      onSuccess();
     } catch (error: unknown) {
       Sentry.captureException(error);
       setNotification(
@@ -93,204 +71,74 @@ export const Form: FC<IProps> = ({ workspace, onSuccess }) => {
     }
   };
 
-  const mintDCX = async (
-    desiredTokenAmount: IDesiredTokenAmount,
-    enoughBalance: ITokenBalance,
-  ) => {
-    const transactions = [];
-    if (!enoughBalance.dcxAllowance) {
-      transactions.push({
-        to: configuration.DC_ADDRESS,
-        value: BigInt(0),
-        data: encodeFunctionData({
-          abi: DimoABI,
-          functionName: 'approve',
-          args: [
-            configuration.DCX_ADDRESS,
-            BigInt(utils.toWei(Math.ceil(Number(desiredTokenAmount.dimo)), 'ether')),
-          ],
-        }),
-      });
-    }
-
-    const balanceDCX = await getCurrentDcxBalance();
-
-    // Call mintInDimo 2 parameteres
-    const dcxAmountInUSD = balanceDCX * Number(DCX_IN_USD);
-    const missingAmount = Math.ceil(
-      Number(desiredTokenAmount.licensePrice) - dcxAmountInUSD,
-    );
-    transactions.push({
-      to: configuration.DCX_ADDRESS,
-      value: BigInt(0),
-      data: encodeFunctionData({
-        abi: DimoCreditsABI,
-        functionName: CONTRACT_METHODS.MINT_IN_DIMO,
-        args: [
-          currentUser!.smartContractAddress,
-          utils.toWei(
-            Math.ceil(missingAmount / Number(desiredTokenAmount.dimoCost)),
-            'ether',
-          ),
-        ],
-      }),
-    });
-    return transactions;
-  };
-
-  const prepareIssueInDC = async (
-    desiredTokenAmount: IDesiredTokenAmount,
-    enoughBalance: ITokenBalance,
-  ) => {
-    if (enoughBalance.dlcAllowance) return [];
-
-    return [
-      {
-        to: configuration.DC_ADDRESS,
-        value: BigInt(0),
-        data: encodeFunctionData({
-          abi: DimoABI,
-          functionName: 'approve',
-          args: [
-            configuration.DLC_ADDRESS,
-            BigInt(utils.toWei(Math.ceil(Number(desiredTokenAmount.dimo)), 'ether')),
-          ],
-        }),
-      },
-    ];
-  };
-
-  const handleCreateWorkspace = async (workspaceData: Partial<IWorkspace>) => {
-    if (!isEmpty(workspace)) return workspace;
-
-    if (!currentUser) throw new Error('There is not organization information');
-
-    setLoadingStatus({
-      label: 'Licensing the application...',
-      status: 'loading',
-    });
-    const workspaceName = workspaceData?.name ?? '';
+  const createDeveloperLicense = async (workspaceData: Partial<IWorkspace>) => {
+    if (!currentUser) throw new Error('User session is invalid');
     setLoadingStatus({
       label: 'Creating developer license...',
       status: 'loading',
     });
-
-    // Call issueInDc 1 parameter
-    const transaction = [
-      {
-        to: configuration.DLC_ADDRESS,
-        value: BigInt(0),
-        data: encodeFunctionData({
-          abi: DimoLicenseABI,
-          functionName: CONTRACT_METHODS.ISSUE_IN_DC,
-          args: [workspaceName],
-        }),
-      },
-    ];
-
-    const { logs } = await processTransactions(transaction);
-    const { topics: [, rawTokenId = '0x', rawOwner = '0x', rawClientId = '0x'] = [] } =
+    const { logs } = await mintLicense(workspaceData?.name ?? '');
+    const { topics: [, rawTokenId = '0x'] = [] } =
       logs?.find(
         ({ topics: [topic = '0x'] = [] }) => topic === configuration.ISSUED_TOPIC,
       ) ?? {};
-
-    return createWorkspace({
-      name: workspaceData.name ?? '',
-      token_id: Number(decodeHex(rawTokenId as `0x${string}`, 'uint256')),
-      client_id: decodeHex(rawClientId as `0x${string}`, 'address'),
-      owner: decodeHex(rawOwner as `0x${string}`, 'address'),
-    });
-  };
-
-  const handleCreateApp = async () => {
-    const { workspace, app } = getValues();
-    const { id: workspaceId = '' } = await handleCreateWorkspace(workspace);
-    const newApp = await createApp(workspaceId, {
-      name: app.name,
-      scope: 'production',
-    });
-    router.push(`/app/details/${newApp.id}`);
-    setNotification('New app created!', 'Success', 'success');
-    onSuccess();
+    return Number(decodeHex(rawTokenId as `0x${string}`, 'uint256'));
   };
 
   if (isLoading) {
     return (
-      <div className={'flex flex-col flex-1 items-center'}>
+      <div className={'flex flex-col flex-1 items-center gap-2'}>
         <Loading className={'!h-9 !w-9 text-primary-200'} />
-        <p className={'text-xl text-center mt-3'}>{loadingStatus?.label ?? 'Loading'}</p>
+        <p className={'text-base font-bold text-center'}>
+          {loadingStatus?.label ?? 'Loading'}
+        </p>
       </div>
     );
   }
   return (
-    <>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        {(!workspace || Object.keys(workspace).length === 0) && (
-          <Label htmlFor="namespace" className="text-sm font-medium">
-            Project Namespace
-            <TextField
-              type="text"
-              placeholder="Project Namespace"
-              {...register('workspace.name', {
-                required: 'This field is required',
-                maxLength: {
-                  value: 32,
-                  message: 'The name should has maximum 32 characters',
-                },
-              })}
-              role="namespace-input"
-            />
-            {errors?.workspace?.name && (
-              <TextError errorMessage={errors?.workspace?.name?.message ?? ''} />
-            )}
-            <p className="text-sm text-text-secondary font-normal">
-              This is the namespace used across all your apps. It is a public name visible
-              to other developers and users in the ecosystem.
-            </p>
-          </Label>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Label htmlFor="namespace" className="text-sm font-medium">
+        Developer License Name
+        <TextField
+          type="text"
+          placeholder="My project"
+          {...register('workspace.name', {
+            required: 'This field is required',
+            maxLength: {
+              value: 32,
+              message: 'The name should has maximum 32 characters',
+            },
+          })}
+          role="namespace-input"
+        />
+        {errors?.workspace?.name && (
+          <TextError errorMessage={errors?.workspace?.name?.message ?? ''} />
         )}
-        <Label htmlFor="name" className="text-sm font-medium">
-          Application Name
-          <TextField
-            type="text"
-            placeholder="Application name"
-            {...register('app.name', {
-              required: 'This field is required',
-              maxLength: {
-                value: 32,
-                message: 'The name should has maximum 32 characters',
-              },
-            })}
-            role="name-input"
-          />
-          {errors?.app?.name && (
-            <TextError errorMessage={errors?.app?.name?.message ?? ''} />
-          )}
-          <p className="text-sm text-text-secondary font-normal">
-            This name is for your reference only
-          </p>
-        </Label>
-        <div className="flex flex-col pt-4 gap-4">
-          <Button
-            type="submit"
-            className="white"
-            role="continue-button"
-            loading={isLoading}
-          >
-            Create application
-          </Button>
-          <Button
-            type="reset"
-            className="dark"
-            role="cancel-button"
-            loading={isLoading}
-            onClick={() => {}}
-          >
-            Cancel
-          </Button>
-        </div>
-      </form>
-    </>
+        <p className="text-sm text-text-secondary font-normal">
+          This is the namespace used across all your apps. It is a public name visible to
+          other developers and users in the ecosystem.
+        </p>
+      </Label>
+      <div className="flex flex-col pt-4 gap-4">
+        <Button
+          type="submit"
+          className="white"
+          role="continue-button"
+          loading={isLoading}
+        >
+          Create application
+        </Button>
+        <Button
+          type="reset"
+          className="dark"
+          role="cancel-button"
+          loading={isLoading}
+          onClick={() => {}}
+        >
+          Cancel
+        </Button>
+      </div>
+    </form>
   );
 };
 
--- src/hooks/useTransactions.ts
+++ src/hooks/useTransactions.ts
@@ -3,8 +3,14 @@ import { encodeFunctionData } from 'viem';
 import DimoLicenseABI from '@/contracts/DimoLicenseContract.json';
 import { useCallback } from 'react';
 import { useContractGA, useGlobalAccount } from '@/hooks';
+import DimoABI from '@/contracts/DimoTokenContract.json';
+import DimoCreditsABI from '@/contracts/DimoCreditABI.json';
+import { IDesiredTokenAmount, ITokenBalance } from '@/types/wallet';
+import { utils } from 'web3';
 
 const { CONTRACT_METHODS } = configuration;
+
+const { DCX_IN_USD = 0.001 } = process.env;
 
 export const useSetRedirectUri = (tokenId: number) => {
   const { validateCurrentSession } = useGlobalAccount();
@@ -73,7 +79,99 @@ export const useEnableSigner = (tokenId: number) => {
   );
 };
 
-export const useIssueInDC = () => {
+const useMintDcx = () => {
+  const { currentUser, getCurrentDcxBalance } = useGlobalAccount();
+  return useCallback(
+    async (desiredTokenAmount: IDesiredTokenAmount, enoughBalance: ITokenBalance) => {
+      const transactions = [];
+      if (!enoughBalance.dcxAllowance) {
+        transactions.push({
+          to: configuration.DC_ADDRESS,
+          value: BigInt(0),
+          data: encodeFunctionData({
+            abi: DimoABI,
+            functionName: 'approve',
+            args: [
+              configuration.DCX_ADDRESS,
+              BigInt(utils.toWei(Math.ceil(Number(desiredTokenAmount.dimo)), 'ether')),
+            ],
+          }),
+        });
+      }
+
+      const balanceDCX = await getCurrentDcxBalance();
+
+      // Call mintInDimo 2 parameteres
+      const dcxAmountInUSD = balanceDCX * Number(DCX_IN_USD);
+      const missingAmount = Math.ceil(
+        Number(desiredTokenAmount.licensePrice) - dcxAmountInUSD,
+      );
+      transactions.push({
+        to: configuration.DCX_ADDRESS,
+        value: BigInt(0),
+        data: encodeFunctionData({
+          abi: DimoCreditsABI,
+          functionName: CONTRACT_METHODS.MINT_IN_DIMO,
+          args: [
+            currentUser!.smartContractAddress,
+            utils.toWei(
+              Math.ceil(missingAmount / Number(desiredTokenAmount.dimoCost)),
+              'ether',
+            ),
+          ],
+        }),
+      });
+      return transactions;
+    },
+    [currentUser, getCurrentDcxBalance],
+  );
+};
+
+export const usePayLicenseFee = () => {
+  const { checkEnoughBalance, getDesiredTokenAmount, processTransactions } =
+    useContractGA();
+  const mintDCX = useMintDcx();
+
+  const prepareIssueInDC = async (
+    desiredTokenAmount: IDesiredTokenAmount,
+    enoughBalance: ITokenBalance,
+  ) => {
+    if (enoughBalance.dlcAllowance) return [];
+    return [
+      {
+        to: configuration.DC_ADDRESS,
+        value: BigInt(0),
+        data: encodeFunctionData({
+          abi: DimoABI,
+          functionName: 'approve',
+          args: [
+            configuration.DLC_ADDRESS,
+            BigInt(utils.toWei(Math.ceil(Number(desiredTokenAmount.dimo)), 'ether')),
+          ],
+        }),
+      },
+    ];
+  };
+
+  return useCallback(async () => {
+    const desiredTokenAmount = await getDesiredTokenAmount();
+    const enoughBalance = await checkEnoughBalance();
+    const transactions = [];
+    if (!enoughBalance.dcx && !enoughBalance.dimo) {
+      return { success: false, reason: 'Insufficient DIMO or DCX balance' };
+    }
+    if (!enoughBalance.dcx) {
+      transactions.push(...(await mintDCX(desiredTokenAmount, enoughBalance)));
+    }
+    transactions.push(...(await prepareIssueInDC(desiredTokenAmount, enoughBalance)));
+    if (transactions.length) {
+      await processTransactions(transactions);
+    }
+    return { success: true };
+  }, [checkEnoughBalance, getDesiredTokenAmount, mintDCX, processTransactions]);
+};
+
+export const useMintLicense = () => {
   const { processTransactions } = useContractGA();
   return useCallback(
     async (licenseName: string) => {
