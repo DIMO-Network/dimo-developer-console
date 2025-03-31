962747da0ec90d7d5493cae3a5aafe3f60005c27
Neil Devas <neil.devas97@gmail.com>
2025-03-31 16:54:46 -0400

    replace with button instead

--- src/app/license/vehicles/[clientId]/layout.tsx
+++ src/app/license/vehicles/[clientId]/layout.tsx
@@ -5,8 +5,8 @@ import { gql } from '@/gql';
 import { useQuery } from '@apollo/client';
 import { Header } from '@/app/license/vehicles/[clientId]/components/Header';
 import { ChevronLeftIcon } from '@heroicons/react/24/outline';
-import Link from 'next/link';
 import { Loader } from '@/components/Loader';
+import { useRouter } from 'next/navigation';
 
 const DEVELOPER_LICENSE_BY_CLIENT_ID_SUMMARY = gql(`
   query DeveloperLicenseByClientIdSummary($clientId: Address!) {
@@ -29,6 +29,12 @@ export default function DeveloperLicenseDetailsPageLayout({
   const { data, loading, error } = useQuery(DEVELOPER_LICENSE_BY_CLIENT_ID_SUMMARY, {
     variables: { clientId },
   });
+  const router = useRouter();
+
+  const onBack = () => {
+    router.replace(`/license/details/${data?.developerLicense.tokenId}`);
+  };
+
   return (
     <div>
       {!!error && <p>Something went wrong fetching the vehicle details</p>}
@@ -36,9 +42,9 @@ export default function DeveloperLicenseDetailsPageLayout({
       {data?.developerLicense && (
         <div className={'flex flex-col'}>
           <div className={'py-4 self-start'}>
-            <Link href={`/license/details/${data.developerLicense.tokenId}`}>
-              <ChevronLeftIcon className={'w-4 h-4 cursor-pointer'} />
-            </Link>
+            <button onClick={onBack}>
+              <ChevronLeftIcon className={'w-4 h-4'} />
+            </button>
           </div>
           <Header
             tokenId={data?.developerLicense.tokenId ?? 0}
