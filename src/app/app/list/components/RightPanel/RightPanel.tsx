350084e0ef6d2dbefd8f82e5c96a908f25cff2e2
Neil Devas <neil.devas97@gmail.com>
2025-04-09 16:43:09 -0400

    start rightpanel

---
+++ src/app/app/list/components/RightPanel/RightPanel.tsx
@@ -0,0 +1,31 @@
+import { FC } from 'react';
+
+import './RightPanel.css';
+import Link from 'next/link';
+
+export const RightPanel: FC = () => {
+  return (
+    <div className={'right-panel-container'}>
+      <WhatsDCX />
+    </div>
+  );
+};
+
+const WhatsDCX = () => {
+  return (
+    <div className={'bg-surface-raised rounded-2xl p-4 w-full flex flex-col gap-2.5'}>
+      <p className={'text-sm font-black'}>What&#39;s DCX?</p>
+      <p className={'text-text-secondary'}>
+        DCX is an abbreviation for DIMO Credits. DIMO Credits cost $0.0001 per credit and
+        API calls and other DIMO fees are priced in DCX.
+      </p>
+      <a
+        target="_blank"
+        href={'https://docs.dimo.org/developer-platform/developer-guide/dimo-credits'}
+        className={'underline'}
+      >
+        Learn more.
+      </a>
+    </div>
+  );
+};
