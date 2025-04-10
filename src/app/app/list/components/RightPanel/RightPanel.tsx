fb7a11572931138b364c17bbc8b9bd57c00d8436
Neil Devas <neil.devas97@gmail.com>
2025-04-10 17:33:04 -0400

    Update RightPanel.tsx

--- src/app/app/list/components/RightPanel/RightPanel.tsx
+++ src/app/app/list/components/RightPanel/RightPanel.tsx
@@ -17,7 +17,7 @@ const WhatsDCX = () => {
     <div className={'bg-surface-raised rounded-2xl p-4 w-full flex flex-col gap-2.5'}>
       <p className={'text-sm font-black'}>What&#39;s DCX?</p>
       <p className={'text-text-secondary'}>
-        DCX is an abbreviation for DIMO Credits. DIMO Credits cost $0.0001 per credit and
+        DCX is an abbreviation for DIMO Credits. DIMO Credits cost $0.001 per credit and
         API calls and other DIMO fees are priced in DCX.
       </p>
       <a
