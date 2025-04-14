f277b65eab8f527b50bbacd4fb5dae29cae886a2
Neil Devas <neil.devas97@gmail.com>
2025-04-14 12:57:03 -0400

    Add developer support button to header (#157)

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,6 +5,7 @@ import { usePathname } from 'next/navigation';
 import { getPageTitle } from '@/config/navigation';
 import { CreditsWidget } from '@/components/CreditsWidget';
 import { AccountInfoButton } from '@/components/AccountInfoButton';
+import { DeveloperSupportButton } from '@/components/DeveloperSupportButton';
 
 export const Header: FC = () => {
   const pathname = usePathname();
@@ -14,6 +15,7 @@ export const Header: FC = () => {
       <p className="page-title">{getPageTitle(pathname) ?? ''}</p>
       <div className="user-information" role="user-information">
         <CreditsWidget />
+        <DeveloperSupportButton variant={'small'} />
         <AccountInfoButton />
       </div>
     </header>
