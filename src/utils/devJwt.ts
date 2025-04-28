a1eef38c3cf997cd27f8bf948319cb81524cf30e
Neil Devas <neil.devas97@gmail.com>
2025-04-28 12:10:40 -0400

    handle decoding jwt

--- src/utils/devJwt.ts
+++ src/utils/devJwt.ts
@@ -1,4 +1,9 @@
-import { getFromLocalStorage, saveToLocalStorage } from '@/utils/localStorage';
+import {
+  getFromLocalStorage,
+  saveToLocalStorage,
+  removeFromLocalStorage,
+} from '@/utils/localStorage';
+import { jwtDecode } from 'jwt-decode';
 
 const getKey = (clientId: string) => `devJwt_${clientId}`;
 
@@ -6,5 +11,18 @@ export const saveDevJwt = (clientId: string, token: string) => {
   return saveToLocalStorage(getKey(clientId), token);
 };
 
-export const getDevJwt = (clientId: string) =>
-  getFromLocalStorage<string>(getKey(clientId));
+export const getDevJwt = (clientId: string) => {
+  const token = getFromLocalStorage<string>(getKey(clientId));
+  if (!token) return null;
+  try {
+    const { exp } = jwtDecode(token);
+    const now = Math.floor(Date.now() / 1000);
+    if (!exp || exp < now) {
+      removeFromLocalStorage(getKey(clientId));
+      return null;
+    }
+    return token;
+  } catch {
+    return null;
+  }
+};
