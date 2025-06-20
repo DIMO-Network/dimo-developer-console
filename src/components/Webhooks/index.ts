c9c87ef2dfb4e40b86ccfcebf75f39b974ec5ea5
Neil Devas <neil.devas97@gmail.com>
2025-06-20 14:10:24 -0400

    Cleanup code

--- src/components/Webhooks/index.ts
+++ src/components/Webhooks/index.ts
@@ -1,5 +1,1 @@
 export * from './WebhooksPage';
-export { LocalDeveloperLicense } from '@/types/webhook';
-export { DevLicenseSelector } from '@/components/Webhooks/components/DeveloperLicenseSelector';
-export { useValidDeveloperLicenses } from '@/components/Webhooks/hooks/useValidDeveloperLicenses';
-export { GenerateDevJWTSection } from '@/components/Webhooks/components/GenerateDevJWTSection';
