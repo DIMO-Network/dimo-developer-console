aa641fec5b5471db65a4ad3670ba10a25b126582
Barrett <barrettkowalsky@gmail.com>
2025-07-17 16:11:05 -0600

    minor connections reformat

--- src/app/connections/components/View/View.tsx
+++ src/app/connections/components/View/View.tsx
@@ -1,12 +1,37 @@
 'use client';
 import React from 'react';
+import { PageSubtitle } from '@/components/PageSubtitle';
+import { Section } from '@/components/Section';
+import { SectionHeader } from '@/components/Section/Header';
+import { Button } from '@/components/Button';
+import { PlusIcon } from '@/components/Icons';
 
 import './View.css';
 
 const View: React.FC = () => {
+  const handleCreateConnection = () => {
+    // TODO: Navigating to next pg w clientId
+    console.log('Clicky');
+  };
+
   return (
     <div className="connections-page">
-      <h2>Hello Connections</h2>
+      <PageSubtitle subtitle="Connection Oracle is an application that performs data streaming from your data source to a DIMO Node." />
+
+      <Section>
+        <SectionHeader title="Connections">
+          <Button className="dark with-icon" onClick={handleCreateConnection}>
+            <PlusIcon className="w-4 h-4" />
+            Create a connection
+          </Button>
+        </SectionHeader>
+
+        <div className="empty-state">
+          <p className="empty-state-message">
+            You haven&apos;t created any connections yet.
+          </p>
+        </div>
+      </Section>
     </div>
   );
 };
