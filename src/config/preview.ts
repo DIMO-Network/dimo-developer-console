fd39319c6bdf64b92e0f558a2273bbb37fba30b5
Barrett <barrettkowalsky@gmail.com>
2025-08-29 11:33:44 -0600

    add stg contract methods

--- src/config/preview.ts
+++ src/config/preview.ts
@@ -5,6 +5,9 @@ export const frontendUrl = 'https://console-staging.dimo.org/';
 export enum CONTRACT_METHODS {
   ISSUE_IN_DC = '0x69054339',
   MINT_IN_DIMO = '0xec88fc37',
+  MINT_CONNECTION = '0xd0def521',
 }
 
 export const identityApiUrl = 'https://identity-api.dev.dimo.zone/query';
+
+export const DCC_ADDRESS = '0x41799E9Dc893722844E771a1C1cAf3BBc2876132'; // Connections
