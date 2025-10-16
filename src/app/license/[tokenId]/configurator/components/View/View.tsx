f3e6b735d24de8dfda91d902c8284c54501b32ed
Eduardo Rodriguez <eduardo.rodriguezz@outlook.com>
2025-10-16 17:59:27 +0200

    [DVR-744] save configuration

--- src/app/license/[tokenId]/configurator/components/View/View.tsx
+++ src/app/license/[tokenId]/configurator/components/View/View.tsx
@@ -23,7 +23,7 @@ const GET_DEVELOPER_LICENSE = gql(`
       ...SignerFragment
       ...RedirectUriFragment
       ...DeveloperLicenseVehiclesFragment
-      ...DeveloperJwtsFragment
+      ...DeveloperJwtsFragment      
     }
   }
 `);
